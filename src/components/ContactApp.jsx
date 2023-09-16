86645fe08fcf559c66f67a385151ea153c3d9dec
Nada Hasni <nadasthing@gmail.com>
2023-09-16 18:15:41 +0700

    feat: simple contact list

---
+++ src/components/ContactApp.jsx
@@ -0,0 +1,13 @@
+import ContactList from "./ContactList";
+import { getData } from "../utils/data";
+
+export default function ContactApp() {
+  const contacts = getData();
+
+  return (
+    <div className="contact-app">
+      <h1>Daftar Kontak</h1>
+      <ContactList contacts={contacts} />
+    </div>
+  );
+}
